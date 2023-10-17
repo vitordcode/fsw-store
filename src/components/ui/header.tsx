b8bdb990d21301136fca54b16f39ec619011ae27
Vitor de Paula <vitordepaula98@hotmail.com>
2023-10-17 19:15:59 -0300

    feat: add header menu using sheet components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,15 +1,50 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react"
-import { Button } from "./button"
-import { Card } from "./card"
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentCircleIcon, PercentIcon, ShoppingCartIcon } from 'lucide-react'
+import { Button } from './button'
+import { Card } from './card'
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
 
 const Header = () => {
   return (
     <Card className="flex justify-between items-center p-7">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
 
-      <h1 className="font-semibold text-lg "><span className="text-primary">FSW</span> Store</h1>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className='mt-4 flex flex-col gap-3'>
+            <Button variant="outline" className="w-full justify-start gap-1">
+              <LogInIcon size={16}/>
+              Fazer login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-1">
+              <HomeIcon size={16}/>
+              Inicio
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-1">
+              <PercentIcon  size={16}/>
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-1">
+              <ListOrderedIcon size={16}/>
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="font-semibold text-lg ">
+        <span className="text-primary">FSW</span> Store
+      </h1>
 
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
