1ff8fa2dd34ac79bba7ae533bc966baf5751580d
Vitor de Paula <vitordepaula98@hotmail.com>
2023-10-20 00:03:40 -0300

    feat: create page Catalog

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,22 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge"
+import { prismaClient } from "@/lib/prisma"
+import { ShapesIcon } from "lucide-react"
+import CategoryItem from "./components/category-item"
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({})
+
   return (
-    <h1>catalog</h1>
+    <div className="p-6">
+      <Badge className="gap-2 uppercase text-base border-2 border-primary px-3 py-1.5" variant="outline">
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-6 mt-4">
+        {categories.map(category => <CategoryItem key={category.id} category={category} />)}
+      </div>
+    </div>
   )
 }
 
