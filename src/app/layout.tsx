b5e516bc60c753690898f4e0892705f407c8c721
Vitor de Paula <vitordepaula98@hotmail.com>
2023-10-19 23:27:42 -0300

    feat: create and add in layout Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/ui/header'
 import { AuthProvider } from '@/providers/auth'
+import Footer from '@/components/ui/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,8 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-        <Header />
-        {children}
+          <Header />
+          {children}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-accent px-8 py-4 mt-8">
+      2023 Copyright FSW Store.
+    </footer>
+  )
+}
+
+export default Footer
